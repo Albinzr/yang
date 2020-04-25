3e66d9d726707a9f7f7d3b78517d6a81f69c38db
Albin CR <Albinzr@gmail.com>
2020-04-26 00:02:43 +0530

    update

--- seed.js
+++ seed.js
@@ -1,3 +1,4 @@
+db = db.getSiblingDB('beacon');
 db.record.createIndex({"sid":1} ,{unique:true}) 
 db.record.createIndex({"createdAt":1} ,{unique:false})
 db.subRecord.createIndex({"sid":1} ,{unique:false})
