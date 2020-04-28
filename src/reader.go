a5378b407784b91f2b83ddb04f3f010e1d0b6b4c
Albin CR <Albinzr@gmail.com>
2020-04-28 09:51:04 +0530

    finished end to end testing

--- src/reader.go
+++ src/reader.go
@@ -66,20 +66,23 @@ func readFromKafka() {
 //each msg enter this func
 //each time a gorutine is creater
 //for commit also eachtime a gorutine is created
+
 func kafkaReaderCallback(reader kafka.Reader, message kafka.Message) {
-	go func() {
-		msgBytes := message.Value
-		var jsonInterface map[string]interface{}
-		json.Unmarshal([]byte(msgBytes), &jsonInterface)
-		var err error
-		if jsonInterface["type"] == "session" {
-			err = dbConfig.Insert("record", jsonInterface)
-		} else if jsonInterface["type"] == "event" {
-			err = dbConfig.Insert("subRecord", jsonInterface)
-		}
-		fmt.Print(".")
-		go commitKafkaMessage(err, reader, message)
-	}()
+	// go func() {
+
+	msgBytes := message.Value
+	var jsonInterface map[string]interface{}
+	json.Unmarshal([]byte(msgBytes), &jsonInterface)
+	var err error
+	if jsonInterface["type"] == "session" {
+		fmt.Println("this is session")
+		err = dbConfig.Insert("record", jsonInterface)
+	} else if jsonInterface["type"] == "event" {
+		err = dbConfig.Insert("subRecord", jsonInterface)
+	}
+
+	commitKafkaMessage(err, reader, message)
+	// }()
 
 }
 
@@ -88,7 +91,7 @@ func commitKafkaMessage(err error, reader kafka.Reader, message kafka.Message) {
 		kafka.Commit(reader, message)
 	} else {
 		//TODO: - if duplicare remove else set up a retry system (3 times) then delete
-		// fmt.Println("err-------->", err)
+		fmt.Println("err-------->", err)
 		kafka.Commit(reader, message)
 	}
 }
