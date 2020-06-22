9b16b26d1f5a11992fc545034ee6a94f07111e03
Albin CR <Albinzr@gmail.com>
2020-06-22 13:26:20 +0530

    update

--- src/reader.go
+++ src/reader.go
@@ -84,8 +84,10 @@ func kafkaReaderCallback(reader kafka.Reader, message kafka.Message) {
 	json.Unmarshal([]byte(msg), &jsonInterface)//error
 
 	if jsonInterface["type"] == nil{
+		fmt.Println("*****************************JSON FAILED**************************")
 		fmt.Println(enMsg[0:2])
 		fmt.Println(msg)
+		fmt.Println("*****************************JSON INFO END**************************")
 	}
 
 	if jsonInterface["type"] == "session" {
