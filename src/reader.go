50b777dcbb2457ad981a9e4a69027668dd2e302a
Albin CR <Albinzr@gmail.com>
2020-07-02 11:33:23 +0530

    update

--- src/reader.go
+++ src/reader.go
@@ -66,37 +66,39 @@ func readFromKafka() {
 
 func kafkaReaderCallback(reader kafka.Reader, message kafka.Message) {
 
-	fmt.Println(".")
-	enMsg := string(message.Value)
-	var err error
-	var msg string
-	if enMsg[0:2] == "en"{
-		msg, err = lz.DecompressFromBase64(enMsg[3:])
-		if err != nil || enMsg == "" {
-			fmt.Println("decomperssion failed*------------------------------------>")
+	go func (){
+		enMsg := string(message.Value)
+		var err error
+		var msg string
+		if enMsg[0:2] == "en"{
+			msg, err = lz.DecompressFromBase64(enMsg[3:])
+			if err != nil || enMsg == "" {
+				fmt.Println("decomperssion failed*------------------------------------>")
+			}
+		}else{
+			msg = enMsg[3:]
 		}
-	}else{
-		msg = enMsg[3:]
-	}
 
-	var jsonInterface map[string]interface{}
-	err = json.Unmarshal([]byte(msg), &jsonInterface)
-	if err != nil {
-		fmt.Println("JSON CONVERSION Failled....................................")
-	}
+		var jsonInterface map[string]interface{}
+		err = json.Unmarshal([]byte(msg), &jsonInterface)
+		if err != nil {
+			fmt.Println("JSON CONVERSION Failled....................................")
+		}
 
-	util.LogInfo("TYPE:",jsonInterface["type"])
+		util.LogInfo("TYPE:",jsonInterface["type"])
 
-	if jsonInterface["type"] == "session" {
-		err = dbConfig.Insert("record", jsonInterface)
-	} else if jsonInterface["type"] == "event" {
-		err = dbConfig.Insert("subRecord", jsonInterface)
-	} else if jsonInterface["type"] == "close" {
-		err = dbConfig.UpdateSession("record", jsonInterface)
-	} else {
-		util.LogInfo("wrong data detected _______________********_______________",len(msg))
-	}
-	commitKafkaMessage(err, reader, message)
+		if jsonInterface["type"] == "session" {
+			err = dbConfig.Insert("record", jsonInterface)
+		} else if jsonInterface["type"] == "event" {
+			err = dbConfig.Insert("subRecord", jsonInterface)
+		} else if jsonInterface["type"] == "close" {
+			err = dbConfig.UpdateSession("record", jsonInterface)
+		} else {
+			util.LogInfo("wrong data detected _______________********_______________",len(msg))
+		}
+		commitKafkaMessage(err, reader, message)
+
+	}()
 
 
 }
