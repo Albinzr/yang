ced3578e049429cd5fc349546817c1710dc401f3
Albin CR <Albinzr@gmail.com>
2020-07-02 22:54:33 +0530

    update

--- src/reader.go
+++ src/reader.go
@@ -68,7 +68,7 @@ func kafkaReaderCallback(reader kafka.Reader, message kafka.Message) {
 		enMsg := string(message.Value)
 		var msg string
 		if enMsg[0:2] == "en"{
-			message, err := lz.DecompressFromBase64("N4IgzglgJiBcIBYAOAxAsgWgG4CECmAnBgKoAMIANCAMbRwhIBOeAtgIYDmE1lIbd8Jq07cMARl5IBIAPS8IAOyh4AHnDFiqAFwCeSPPTxY8Crby0QWeMFrYsk6gKwEATAA4xHxwGZSpAGxUUGy2cADaoBZW6h5u3i7+AHQIAOyOVCwA9gCuYHgAGuoIcRk5eQCacP6+2noG8Fm5eGiZxrwKbFgQHCEQmQoAKnVwpAC+FJGW9Z6eKSkuiWLepU2FsGLFyyCNFVUuKbX69DvNrQZUHV09Fv1DR7BjEyBR07FupASJ3gfbZQVFJV+TUqsEcbgQh3qQLyLTaF063V6t2GD3Gk2i6ze/lIiRcmmh/3WqQhBJBCH8BEhxz+sPOIEuiJugxRYwAuqMgA==")
+			message, err := lz.DecompressFromBase64("N4IgzglgJiBcIBYAOAxAsgWgG4CECmAnBgKoAMIANCAMbRwhIBOeAtgIYDmE1lIbd8Jq07cMARl5IBIAPS8IAOyh4AHnDFiqAFwCeSPPTxY8Crby0QWeMFrYsk6gKwEATAA4xHxwGZSpAGxUUGy2cADaoBZW6h5u3i7+AHQIAOyOVCwA9gCuYHgAGuoIcRk5eQCacP6+2noG8Fm5eGiZxrwKbFgQHCEQmQoAKnVwpAC+FJGW9Z6eKSkuiWLepU2FsGLFyyCNFVUuKbX69DvNrQZUHV09Fv1DR7BjEyBR07FupASJ3gfbZQVFJV+TUqsEcbgQh3qQLyLTaF063V6t2GD3Gk2i6ze/lIiRcmmh/3WqQhBJBCH8BEhxz+sPOIEuiJugxRYwAuqMgA=="
 			msg = message
 			if err != nil || enMsg == "" {
 				fmt.Println("decomperssion failed*------------------------------------>")
@@ -78,24 +78,24 @@ func kafkaReaderCallback(reader kafka.Reader, message kafka.Message) {
 		}
 		util.LogInfo(len(msg))
 		//
-		//var jsonInterface map[string]interface{}
-		//err := json.Unmarshal([]byte(msg), &jsonInterface)
-		//if err != nil {
-		//	fmt.Println("JSON CONVERSION Failled....................................")
-		//}
-		//
-		//util.LogInfo("TYPE:",jsonInterface["type"])
-		//
-		//if jsonInterface["type"] == "session" {
-		//	err = dbConfig.Insert("record", jsonInterface)
-		//} else if jsonInterface["type"] == "event" {
-		//	err = dbConfig.Insert("subRecord", jsonInterface)
-		//} else if jsonInterface["type"] == "close" {
-		//	err = dbConfig.UpdateSession("record", jsonInterface)
-		//} else {
-		//	util.LogInfo("wrong data detected _______________********_______________",len(msg))
-		//}
-		//commitKafkaMessage(err, reader, message)
+		var jsonInterface map[string]interface{}
+		err := json.Unmarshal([]byte(msg), &jsonInterface)
+		if err != nil {
+			fmt.Println("JSON CONVERSION Failled....................................")
+		}
+
+		util.LogInfo("TYPE:",jsonInterface["type"])
+
+		if jsonInterface["type"] == "session" {
+			err = dbConfig.Insert("record", jsonInterface)
+		} else if jsonInterface["type"] == "event" {
+			err = dbConfig.Insert("subRecord", jsonInterface)
+		} else if jsonInterface["type"] == "close" {
+			err = dbConfig.UpdateSession("record", jsonInterface)
+		} else {
+			util.LogInfo("wrong data detected _______________********_______________",len(msg))
+		}
+		commitKafkaMessage(err, reader, message)
 
 }
 
