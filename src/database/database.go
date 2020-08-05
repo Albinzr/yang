091ac54171299237d9cf5a2fec373152a0fb4653
Albin CR <Albinzr@gmail.com>
2020-08-06 00:18:21 +0530

    url test

--- src/database/database.go
+++ src/database/database.go
@@ -150,11 +150,11 @@ func (c *Config) UpdateSessionArrays(collectionName string, jsonInterface map[st
 		}
 
 		if url, err := jsonInterface["url"].(string); err {
-			updateSet["$project"] = bson.M{
-				"entryUrl": bson.M{
-					"$ifNull": []interface{}{"$entryUrl", url},
-				},
-			}
+			// updateSet["$project"] = bson.M{
+			// 	"entryUrl": bson.M{
+			// 		"$ifNull": []interface{}{"$entryUrl", url},
+			// 	},
+			// }
 			updateSet["$push"] = bson.M{
 				"urls": url,
 			}
