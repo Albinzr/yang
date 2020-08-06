0601071e0abef103eb9274b8d9a3d3e1b194a94b
Albin CR <Albinzr@gmail.com>
2020-08-06 14:24:21 +0530

    url test

--- src/database/database.go
+++ src/database/database.go
@@ -104,7 +104,7 @@ func (c *Config) UpdateSessionUserInfo(collectionName string, jsonInterface map[
 			updateSet = append(updateSet, primitive.E{Key: "username", Value: username})
 		}
 		if id := jsonInterface["uuid"]; id != nil {
-			updateSet = append(updateSet, primitive.E{Key: "id", Value: id})
+			updateSet = append(updateSet, primitive.E{Key: "userId", Value: id})
 		}
 		if sex := jsonInterface["sex"]; sex != nil {
 			updateSet = append(updateSet, primitive.E{Key: "sex", Value: sex})
@@ -151,24 +151,24 @@ func (c *Config) UpdateSessionArrays(collectionName string, jsonInterface map[st
 
 		if url, err := jsonInterface["url"].(string); err {
 			// updateSet["$setOnInsert"] = bson.M{"entryUrl": url}
-			// updateSet["$push"] = bson.M{"urls": url}
-			// updateSet["$set"] = bson.M{"exitUrl": url}
+			updateSet["$push"] = bson.M{"urls": url}
+			updateSet["$set"] = bson.M{"exitUrl": url}
 
-			updateSet = bson.M{
-				"$setOnInsert": bson.M{
-					"createdat": url,
-				},
-				"$currentDate": bson.M{
-					"updatedat": true,
-				},
-				"$set": bson.M{
-					"name":       url,
-					"linenumber": url,
-				},
-				"$addToSet": bson.M{
-					"sources": url,
-				},
-			}
+			// updateSet = bson.M{
+			// 	"$setOnInsert": bson.M{
+			// 		"createdat": url,
+			// 	},
+			// 	"$currentDate": bson.M{
+			// 		"updatedat": true,
+			// 	},
+			// 	"$set": bson.M{
+			// 		"name":       url,
+			// 		"linenumber": url,
+			// 	},
+			// 	"$addToSet": bson.M{
+			// 		"sources": url,
+			// 	},
+			// }
 
 		}
 
