375714147e2e8af273cb91506d4714796e1ece8e
Albin CR <Albinzr@gmail.com>
2020-08-09 21:51:07 +0530

    update

--- src/database/database.go
+++ src/database/database.go
@@ -20,6 +20,41 @@ type Config struct {
 	ctx      context.Context
 }
 
+// Tests :- afsdfdfdf
+func (c *Config) Tests() error {
+
+	m := make(map[string]interface{})
+	m["kid"] = 1
+	c.database.Collection("test").InsertOne(c.ctx, m)
+
+	// updateSet := bson.M{
+	// 	"$push": bson.M{
+	// 		"url": "url://",
+	// 	},
+	// 	"$set": bson.M{
+	// 		"name": "2342344",
+	// 	},
+	// 	"$setOnInsert": bson.M{
+	// 		"rol": "2342344",
+	// 	},
+	// }
+
+	updateSet := bson.M{
+		"$push": bson.M{
+			"urls": "https://www.premagic.com/1",
+		},
+		"$set": bson.M{
+			"entryUrl": "https://www.premagic.com/3",
+			"exitUrl":  "https://www.premagic.com/2",
+		},
+	}
+	r, err := c.database.Collection("test").UpdateOne(c.ctx, bson.M{"kid": 1}, updateSet)
+
+	fmt.Println(r, err, "*****tests..........", updateSet)
+
+	return err
+}
+
 //Init :- initalize function
 func (c *Config) Init() error {
 	var err error
@@ -51,28 +86,28 @@ func LogError(message string, errorData error) {
 //UpdateSession :-  database insert
 func (c *Config) UpdateSession(collectionName string, jsonInterface map[string]interface{}) error {
 	//TODO: - add sid and aid in search query connect using $and
-	fmt.Println("in")
 	sid := jsonInterface["sid"]
+	ip := jsonInterface["ip"]
+	startTime := int64(jsonInterface["startTime"].(float64))
+	endTime := int64(jsonInterface["endTime"].(float64))
+	initial := jsonInterface["initial"].(bool)
+
+	// errorCount := int(jsonInterface["errorCount"].(float64))
+	// clickCount := int(jsonInterface["clickCount"].(float64))
+	// pageCount := int(jsonInterface["pageCount"].(float64))
 
 	searchQuery := bson.D{
 		primitive.E{Key: "sid", Value: sid},
 	}
 
-	ip := jsonInterface["ip"]
-	startTime := int64(jsonInterface["startTime"].(float64))
-	endTime := int64(jsonInterface["endTime"].(float64))
-	initial := jsonInterface["initial"].(bool)
-	// errorCount := int(jsonInterface["errorCount"].(float64))
-	// clickCount := int(jsonInterface["clickCount"].(float64))
-	// pageCount := int(jsonInterface["pageCount"].(float64))
-
 	updateSet := bson.D{
 		primitive.E{Key: "ip", Value: ip},
 		primitive.E{Key: "endTime", Value: endTime},
-		// 	primitive.E{Key: "errorCount", Value: errorCount},
-		// 	primitive.E{Key: "clickCount", Value: clickCount},
-		// 	primitive.E{Key: "pageCount", Value: pageCount},
 	}
+
+	// primitive.E{Key: "errorCount", Value: errorCount},
+	// primitive.E{Key: "clickCount", Value: clickCount},
+	// primitive.E{Key: "pageCount", Value: pageCount},
 
 	if initial {
 		updateSet = append(updateSet, primitive.E{Key: "startTime", Value: startTime})
@@ -87,7 +122,7 @@ func (c *Config) UpdateSession(collectionName string, jsonInterface map[string]i
 	_, err := c.database.Collection(collectionName).UpdateOne(c.ctx, searchQuery, updateData)
 
 	if err != nil {
-		fmt.Println("cannot insert to db with search query:", searchQuery, "options", updateData, "Incoming Data", jsonInterface)
+		fmt.Println("cannot insert to db with search query:", searchQuery, "options", updateData)
 	}
 	return err
 }
